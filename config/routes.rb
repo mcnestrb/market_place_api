db08343fd3918d82f0011530683b8e3666cdfce3
Brian McNestry <brianmcnestry@gmail.com>
2016-09-18 18:24:29 +0100

    set up tests for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
-MarketPlaceApi::Application.routes.draw do
-	# Api definition
-	namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do 
+require 'api_constraints'
 
-	end
+MarketPlaceApi::Application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
+  end
 end
